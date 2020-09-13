1c284522fce969f14cc98e5735513d65b07644d8
Chayut Ruksomya <chayut.r@bluepi.co.th>
2020-09-13 12:39:40 +0700

    Split into test context

--- src/components/GuessWordList/GuessedWordList.spec.tsx
+++ src/components/GuessWordList/GuessedWordList.spec.tsx
@@ -3,7 +3,7 @@ import { shallow, ShallowWrapper } from 'enzyme';
 import GuessedWordList, { GuessedWordListProps } from './GuessedWordList';
 import { findByTesttAttr } from '../../../test/test-utils';
 
-describe('<Congrats />', () => {
+describe('<GuessedWordList />', () => {
   let guessedWordListWrapper: ShallowWrapper;
   let defaultProps: GuessedWordListProps = {
     guessedWords: [],
@@ -13,11 +13,35 @@ describe('<Congrats />', () => {
     guessedWordListWrapper = shallow(<GuessedWordList {...defaultProps} />);
   });
 
-  it('render component', () => {
-    expect(guessedWordListWrapper).toMatchSnapshot();
-    const containerComponent = findByTesttAttr(guessedWordListWrapper, 'component-guessed-word-list');
-    expect(containerComponent).toHaveLength(1);
-  });
+  describe('if there are no words guessed', () => {
+    beforeEach(() => {
+      guessedWordListWrapper.setProps({ guessedWords: [] });
+    });
+
+    it('render the component without error', () => {
+      expect(guessedWordListWrapper).toMatchSnapshot();
+      const containerComponent = findByTesttAttr(guessedWordListWrapper, 'component-guessed-word-list');
+      expect(containerComponent).toHaveLength(1);
+    });
+  })
+
+  describe('if there are words guessed', () => {
+    beforeEach(() => {
+      guessedWordListWrapper.setProps({
+        guessedWords: [{
+          guessedWord: 'qwerty',
+          letterMatchCount: 2,
+        }]
+      });
+    });
+
+    it('render the component without error', () => {
+      expect(guessedWordListWrapper).toMatchSnapshot();
+      const containerComponent = findByTesttAttr(guessedWordListWrapper, 'component-guessed-word-list');
+      expect(containerComponent).toHaveLength(1);
+    });
+  })
+
 
 })
 
